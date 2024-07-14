2f01a270e5e80dd53f41dc66a5a27a8ac5960125
GabrielaGuestory <160191957+GabrielaGuestory@users.noreply.github.com>
2024-07-14 10:55:39 -0300

    Add files via upload
    Decodificador-de-Texto
    Este projeto trata-se de uma aplicação web simples que criptografa e descriptografa textos utilizando um método específico de substituição de letras.
    Ele permite que você troque mensagens secretas com outras pessoas que saibam o segredo da criptografia utilizada.
    Descrição - Regras de Criptografia:
    A letra "e" é convertida para "enter"
    A letra "i" é convertida para "imes"
    A letra "a" é convertida para "ai"
    A letra "o" é convertida para "ober"
    A letra "u" é convertida para "ufat"
    Requisitos:
    A aplicação deve funcionar apenas com letras minúsculas.
    Não devem ser utilizados letras com acentos nem caracteres especiais.
    Deve ser possível converter uma palavra para a versão criptografada e também retornar uma palavra criptografada para a versão original.
    Por exemplo:
    "gato" => "gaitober"
    "gaitober" => "gato"
    Ferramentas Utilizadas
    Visual Studio Code: Editor de código usado para desenvolver o projeto.
    HTML: Estrutura básica da página web.
    CSS: Estilização da página web.

---
+++ Decodificador-de-Texto/function encryptText() {.js
@@ -0,0 +1,30 @@
+function encryptText() {
+    const inputText = document.getElementById('input-text').value;
+    const encryptedText = inputText
+        .replace(/e/g, 'enter')
+        .replace(/i/g, 'imes')
+        .replace(/a/g, 'ai')
+        .replace(/o/g, 'ober')
+        .replace(/u/g, 'ufat');
+    document.getElementById('output-text').value = encryptedText;
+}
+
+
+function decryptText() {
+    const inputText = document.getElementById('input-text').value;
+    const decryptedText = inputText
+        .replace(/enter/g, 'e')
+        .replace(/imes/g, 'i')
+        .replace(/ai/g, 'a')
+        .replace(/ober/g, 'o')
+        .replace(/ufat/g, 'u');
+    document.getElementById('output-text').value = decryptedText;
+}
+
+
+function copyToClipboard() {
+    const outputText = document.getElementById('output-text');
+    outputText.select();
+    document.execCommand('copy');
+    alert('Texto copiado para a área de transferência!');
+}
